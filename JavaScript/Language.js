0d3707a2fecba3efbd9fcea658010bf66a34d944
Limpin0123 <120259965+Limpin0123@users.noreply.github.com>
2025-11-19 09:10:27 +0100

    Test Lang trad

---
+++ JavaScript/Language.js
@@ -0,0 +1,24 @@
+function loadLanguage(lang) {
+    Promise.all([
+        fetch(`./Json/lang/${lang}/HeaderFooterJson.json`).then(r => r.json()),
+        fetch(`./Json/lang/${lang}/${document.body.dataset.page}.json`).then(r => r.json())//body must be like => <body data-page="nameOfTheJson">
+    ])
+    .then(([HeaderFooter, pageData]) => {
+        const translations = {...HeaderFooter, ...pageData};
+      document.querySelectorAll("[data-i18n]").forEach(element => {
+        const key = element.getAttribute("data-i18n");
+        if (translations[key]) {
+          element.textContent = translations[key];
+        }
+      });
+    })
+    .catch(err => console.error("Error when loading language :", err));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadLanguage("fr");
+
+  document.getElementById("button-fr").addEventListener("click", () => loadLanguage("fr"));
+  document.getElementById("button-en").addEventListener("click", () => loadLanguage("en"));
+});
+//Page must include the line : <script src="./JavaScript/Language.js"></script>
